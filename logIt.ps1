757361e7d086809ad242972b3f31c9f3df044043
Tom Dobson <dobson980@gmail.com>
2018-03-06 11:03:35 -0800

    Update logIt.ps1

--- logIt.ps1
+++ logIt.ps1
@@ -1,11 +1,12 @@
-﻿<#
+#region Log-It Function
+<#
 .SYNOPSIS
   Function to write logs to HTML file for general error handling @ Sharp HealthCare.
 .DESCRIPTION
   Standardized Log Writting Function for Sharp HealthCare
 .PARAMETER <Parameter_Name>
   -Level (Mandatory)
-        Available Levels : INFO WARN ERROR FATAL DEBUG SUCCES
+        Available Levels : INFORM WARN ERROR FATAL DEBUG SUCCES
   -Message (Mandatory)
         String containing Message to write to log
   -LogName (Mandatory)
@@ -18,124 +19,178 @@
   Version:        1.0
   Author:         Thomas Dobson
   Creation Date:  12/12/2017
-  Purpose/Change: Initial script development
+  Change Date:    3/6/2017
+  Purpose/Change: Improved Logging. Bug Fixes.
   
 .EXAMPLE
-  Log-It "WARN" "INSTALL FAILED. UNABLE TO WRITE TO DIRECTORY" "MyInstaller"
+  Log-It "WARN" "INSTALL FAILED. UNABLE TO WRITE TO DIRECTORY""MyInstaller"
   Log-IT "SUCCESS" "File Copied SuccessFully." "MyInstaller" "IS1713922"
+  Log-IT "FATAL" "This Error Happened" "MyScript" -intializeNewLog $true
 #>
-Function Log-It {
-
+Function Log-It
+{
+	
 	[CmdletBinding()]
 	Param (
 		[Parameter(Mandatory = $true, Position = 0, HelpMessage = "Log Level")]
-		[ValidateSet("INFO", "WARN", "ERROR", "FATAL", "DEBUG", "SUCCESS")]
-		[string]$Level = "INFO",
+		[ValidateSet("INFORM", "WARN", "ERROR", "FATAL", "DEBUG", "SUCCESS")]
+		[string]$Level = "INFORM",
 		[Parameter(Mandatory = $true, Position = 1, HelpMessage = "Message to be written to the log")]
 		[string]$Message,
 		[Parameter(Mandatory = $true, Position = 2, HelpMessage = "Log file location and name")]
 		[string]$LogName,
-        [Parameter(Mandatory = $false, Position = 3, HelpMessage = "Target PC Asset Tag / Hostname")]
-        [String]$computerName
+		[Parameter(Mandatory = $false, Position = 4, HelpMessage = "Target PC Asset Tag / Hostname")]
+		[Bool]$intializeNewLog = $false,
+		[Parameter(Mandatory = $false, Position = 3, HelpMessage = "Target PC Asset Tag / Hostname")]
+		[String]$computerName
 	)
-
-BEGIN {
-
-$initiatingUser = [Security.Principal.WindowsIdentity]::GetCurrent().Name
-
-$html = @"
+	
+	#region variables
+	$initiatingUser = [Security.Principal.WindowsIdentity]::GetCurrent().Name
+	
+	$html = @"
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
 <title>$logName</title>
 <link rel="stylesheet" href="C:\LDLogs\Css\logstyle.css">
-<script src="script.js"></script>
 </head>
 <body>
-<span class=`"InitHead`">$Stamp $logName Initialied by $initiatingUser</span><br>
 </body></html>
 "@
+	
+	$css = @"
+/*v1.1*/
 
-$css = @"
-.INFO{
+#INFORM{
   font: 20px;
-  font-weight: bold;
-  color: Black;
+  font-weight: bolder;
+  color: BLACK;
 }
-.WARN{
+
+#WARN{
   font: 20px;
-  font-weight: bold;
-  color: RED;
-}
-.ERROR{
-  font: 20px;
-  font-weight: bold;
-  color: RED;
-}
-.FATAL{
-  font: 20px;
-  font-weight: bold;
-  color: RED;
-}
-.DEBUG{
-  font: 20px;
-  font-weight: bold;
+  font-weight: bolder;
   color: ORANGE;
 }
-.SUCCESS{
+
+#ERROR{
   font: 20px;
-  font-weight: bold;
+  font-weight: bolder;
+  color: RED;
+}
+#FATAL{
+  font: 20px;
+  font-weight: bolder;
+  color: RED;
+}
+
+#DEBUG{
+  font: 20px;
+  font-weight: bolder;
+  color: BLUE;
+}
+
+#SUCCESS{
+  font: 20px;
+  font-weight: bolder;
   color: GREEN;
 }
 
-.InitHead{
-  font-size: 150%;
+#header{
+  font-size: 1.5em;
   font-weight: bold;
 }
 "@
-
-    	$Stamp = (Get-Date).toString("HH:mm:ss MM/dd/yyyy")
-
-        If(!$computerName){
-            $computerName = $env:computername
-        }
-
-        $logPath = "\\$computerName\C$\LDLogs\$LogName.html"
-        $CSSPath = "\\$computerName\C$\LDLogs\css\logstyle.css"
-
-        If(!(Test-Path($logPath))) {
-            New-Item -Path "\\$computerName\C$\LDLogs" -Name "$LogName.html" -ItemType File
-            $html | Out-File $logPath
-        }
-
-         If(!(Test-Path($CSSPath))) {
-            New-Item -Path "\\$computerName\C$\LDLogs\CSS" -Name "logstyle.css" -ItemType File
-            $css | Out-File $CSSPath
-        }
-
-    }
-    PROCESS {
-    	If ($logPath) { 
-
-            Switch($Level) {
-                "INFO" {AppendToHTMLLog}
-                "WARN" {AppendToHTMLLog}
-                "ERROR" {AppendToHTMLLog}
-                "FATAL" {AppendToHTMLLog}
-                "DEBUG" {AppendToHTMLLog}
-                "SUCCESS" {AppendToHTMLLog}
-            }
-	    } Else {
-		    Write-Output $Line
-	    }
-    }
-    END {}   
+	
+	$Stamp = (Get-Date).toString("HH:mm:ss MM/dd/yyyy")
+	
+	If (!$computerName)
+	{
+		$computerName = $env:computername
+	}
+	
+	$logPath = "\\$computerName\C$\LDLogs\$LogName.html"
+	$CSSPath = "\\$computerName\C$\LDLogs\css\logstyle.css"
+	
+	#endregion
+	
+	Function AppendToHTMLLog
+	{
+		
+		$htmlClosure = "</body></html>"
+		$htmlHeader = "<p id=`"header`">[$Stamp] $logName Initialied by $initiatingUser</p>"
+		$htmlSpacers = "<p>****************************************************************************</p>"
+		$htmlLogger = "[$Stamp]<span id=`"$level`">&nbsp&nbsp$level&nbsp&nbsp</span> $message<br>"
+		
+		$rawLog = Get-Content $logPath
+		if ($intializeNewLog)
+		{
+			$rawLog.Replace("</body></html>", "$htmlSpacers$htmlHeader$htmlSpacers$htmlLogger$htmlClosure") | Out-File $logPath
+		}
+		else
+		{
+			$rawLog.Replace("</body></html>", "$htmlLogger$htmlClosure") | Out-File $logPath
+		}
+		
+	}
+	
+	#Generate New CSS
+	function createCSS
+	{
+		New-Item -Path "\\$computerName\C$\LDLogs\CSS" -Name "logstyle.css" -ItemType File
+		$css | Out-File $CSSPath
+	}
+	
+	
+	#If specified logfile doesn't exist; create it.
+	If (!(Test-Path($logPath)))
+	{
+		New-Item -Path "\\$computerName\C$\LDLogs" -Name "$LogName.html" -ItemType File
+		$html | Out-File $logPath
+	}
+	
+	#check logfile versioning. Remove old logs. Generate / Replace CSS.
+	$cssExists = Test-Path($CSSPath)
+	if ($cssExists)
+	{
+		
+		$cssversion = (Get-Content C:\ldlogs\css\logstyle.css -First 1).Substring(3, 3)
+		$currentVersion = "1.0"
+		if ($cssversion -ne $currentVersion)
+		{
+			Remove-Item -Path $CSSPath -Force
+			createCSS
+		}
+		
+	}
+	else
+	{
+		createCSS
+	}
+	
+	#Write to Logs
+	If ($logPath)
+	{
+		
+		Switch ($Level)
+		{
+			"INFORM" { AppendToHTMLLog }
+			"WARN" { AppendToHTMLLog }
+			"ERROR" { AppendToHTMLLog }
+			"FATAL" { AppendToHTMLLog }
+			"DEBUG" { AppendToHTMLLog }
+			"SUCCESS" { AppendToHTMLLog }
+		}
+	}
+	Else
+	{
+		Write-Output $Line
+	}
+	
 }
 
-Function AppendToHTMLLog {
-            
-$rawLog = Get-Content $logPath
-$rawLog.Replace("</body></html>","$Stamp <span class=`"$level`">$level</span> $message<br></body></html>") | Out-File $logPath
 
-}
+
+#endregion
